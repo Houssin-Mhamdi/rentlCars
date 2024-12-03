588f50ddf374e51a634f3982571d500b46ee0b88
houssinCarnelian <houssin.carnelian@gmail.com>
2024-12-03 21:05:33 +0100

    versel config

--- ormConfig.ts
+++ ormConfig.ts
@@ -1,4 +1,3 @@
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { Product } from 'src/product/entities/product.entity';
 import { User } from 'src/user/entities/user.entity';
 import { Car } from 'src/car/entities/car.entity';
@@ -6,13 +5,16 @@ import { Booking } from 'src/booking/entities/booking.entity';
 import { Contact } from 'src/contact/entities/cantact.entity';
 const config: any = {
   type: 'postgres',
-  database: 'testDB',
-  host: 'localhost',
+  host: 'dpg-ct72tplds78s73bc6je0-a.oregon-postgres.render.com', // Updated host
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
+  username: 'car_rent_jizy_user',
+  password: 'ewW8CXozwK7g38jkY05V4NcWQgA0ha3d',
+  database: 'car_rent_jizy',
   entities: [Product, User, Car, Booking, Contact],
-  synchronize: false,
+  synchronize: true,
   autoLoadEntities: true,
+  ssl: {
+    rejectUnauthorized: false, // Required for many cloud-hosted Postgres databases
+  },
 };
 export default config;
