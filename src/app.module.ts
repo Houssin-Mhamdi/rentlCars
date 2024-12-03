a70cf00801264147d94e3708757343ebf5479206
houssinCarnelian <houssin.carnelian@gmail.com>
2024-12-03 21:07:14 +0100

    versel config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,26 @@ import { BookingModule } from './booking/booking.module';
 import config from '../ormconfig';
 import { ContactModule } from './contact/contact.module';
 @Module({
-  imports: [ProductModule, TypeOrmModule.forRoot(config), UserModule, CarModule, BookingModule,ContactModule],
+  imports: [
+    ProductModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dpg-ct72tplds78s73bc6je0-a.oregon-postgres.render.com',
+      port: 5432,
+      username: 'car_rent_jizy_user',
+      password: 'ewW8CXozwK7g38jkY05V4NcWQgA0ha3d',
+      database: 'car_rent_jizy',
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+    UserModule,
+    CarModule,
+    BookingModule,
+    ContactModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
