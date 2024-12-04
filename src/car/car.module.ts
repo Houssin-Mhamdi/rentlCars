d1dab89540c55651d0667ca49d4c6e37cf1121e5
houssin.m <houssin.m@frenchinnov.fr>
2024-12-04 14:10:22 +0100

    add the quantity and the logic for update the state

--- src/car/car.module.ts
+++ src/car/car.module.ts
@@ -3,9 +3,13 @@ import { CarService } from './car.service';
 import { CarController } from './car.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Car } from './entities/car.entity';
+import { Booking } from 'src/booking/entities/booking.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Car])],
+  imports: [
+    TypeOrmModule.forFeature([Car]),
+    TypeOrmModule.forFeature([Booking]),
+  ],
   controllers: [CarController],
   providers: [CarService],
 })
